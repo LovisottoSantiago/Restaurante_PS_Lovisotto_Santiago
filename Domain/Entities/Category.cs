ca72ffa68abe557a2fbb81d565ff13fe51a707e0
LovisottoSantiago <santiagolovisotto@gmail.com>
2025-08-28 23:14:41 -0300

    Creacion del proyecto, primera migracion y data seeding inicial

---
+++ Domain/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Order { get; set; }
+
+        public ICollection<Dish> Dishes { get; set; }
+    }
+}
---
+++ Domain/Entities/OrderItem.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Entities
+{
+    public class OrderItem
+    {
+        public long OrderItemId { get; set; }
+        public Order Order { get; set; }
+        public long OrderId { get; set; }
+        public Dish Dish { get; set; }
+        public Guid DishId { get; set; }
+        public int Quantity { get; set; }
+        public string Notes { get; set; }
+        public Status Status { get; set; }
+        public int StatusId { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
